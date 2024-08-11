84353d6928e6b882e46e870088b91adc5b93bdb8
dedelstein <danhedelstein@gmail.com>
2024-08-11 20:22:49 +0300

    images and styling

--- app/page.tsx
+++ app/page.tsx
@@ -7,67 +7,67 @@ import wedding_landing from '@/public/wedding_landing_sm.jpg'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div className="z-10 w-full max-w-2xl items-center justify-between font-mono text-sm lg:flex relative">
-      <form>
-        <Image
-          src={wedding_landing}
-          layout="responsive"
-          alt="Wedding Landing Image"
-          width={1509}
-          height={2523}
-        />
-        <button
-          formAction={login}
+      <div className="z-10 w-full max-w-2xl items-center justify-between font-mono text-sm lg:flex relative">
+        <form>
+          <Image
+            src={wedding_landing}
+            layout="responsive"
+            alt="Wedding Landing Image"
+            width={1509}
+            height={2523}
+          />
+          <button
+            formAction={login}
+            style={{
+              position: 'absolute',
+              top: '46%',
+              left: '43%',
+              width: '15%', // Set the width to match the area you want to cover
+              height: '5%', // Set the height to match the area you want to cover
+              background: 'transparent', // Make the button invisible
+              border: 'none',
+              cursor: 'pointer',
+            }}
+            aria-label='RSVP'
+          />
+        </form>
+        <Link href={'/directions'}
           style={{
             position: 'absolute',
-            top: '46%',
-            left: '43%',
-            width: '15%', // Set the width to match the area you want to cover
+            top: '52%',
+            left: '35%',
+            width: '31%', // Set the width to match the area you want to cover
             height: '5%', // Set the height to match the area you want to cover
             background: 'transparent', // Make the button invisible
             border: 'none',
             cursor: 'pointer',
           }}
-          aria-label='RSVP'
-        />          
-      </form>
-      <Link href={'/directions'}
-        style={{
-          position: 'absolute',
-          top: '52%',
-          left: '35%',
-          width: '31%', // Set the width to match the area you want to cover
-          height: '5%', // Set the height to match the area you want to cover
-          background: 'transparent', // Make the button invisible
-          border: 'none',
-          cursor: 'pointer',
-        }}
-        aria-label='Directions'/>
-      <Link href={'/schedule'}
-        style={{
-          position: 'absolute',
-          top: '58%',
-          left: '37%',
-          width: '28%', // Set the width to match the area you want to cover
-          height: '5%', // Set the height to match the area you want to cover
-          background: 'transparent', // Make the button invisible
-          border: 'none',
-          cursor: 'pointer',
-        }}
-        aria-label='Schedule'
+          aria-label='Directions' />
+        <Link href={'/schedule'}
+          style={{
+            position: 'absolute',
+            top: '58%',
+            left: '37%',
+            width: '28%', // Set the width to match the area you want to cover
+            height: '5%', // Set the height to match the area you want to cover
+            background: 'transparent', // Make the button invisible
+            border: 'none',
+            cursor: 'pointer',
+          }}
+          aria-label='Schedule'
         />
         <Link href={'/stay'}
-        style={{
-          position: 'absolute',
-          top: '64%',
-          left: '29%',
-          width: '43%', // Set the width to match the area you want to cover
-          height: '5%', // Set the height to match the area you want to cover
-          background: 'transparent', // Make the button invisible
-          border: 'none',
-          cursor: 'pointer',
-        }}
-        aria-label='Accommodations'
+          style={{
+            position: 'absolute',
+            top: '64%',
+            left: '29%',
+            width: '43%', // Set the width to match the area you want to cover
+            height: '5%', // Set the height to match the area you want to cover
+            background: 'transparent', // Make the button invisible
+            border: 'none',
+            cursor: 'pointer',
+          }}
+          aria-label='Accommodations'
         />
       </div>
     </main>
--- app/stay/page.tsx
+++ app/stay/page.tsx
@@ -1,12 +1,18 @@
 'use client';
 
 import Link from 'next/link'
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react'
 
 import styles from './Stay.module.css'
 import { Manrope } from 'next/font/google'
 import CustomPopup from '@/components/custom_popup'
+
+import dog_dwg from '@/public/dink_dwg.jpg'
+import yurt_dwg from '@/public/yurt_dwg.jpg'
+import motel_dwg from '@/public/motel_dwg.jpg'
+import tent_dwg from '@/public/tent_dwg.jpg'
 
 const manrope = Manrope({ subsets: ['latin'] })
 
@@ -33,6 +39,7 @@ export default function Stay() {
     router.push('/modify');
   };
 
+
   return (
     <div className={manrope.className}>
       <div className={styles.container}>
@@ -43,10 +50,18 @@ export default function Stay() {
         </p>
 
         <h2 className={styles.subtitle}>Yurts</h2>
-        <p className={styles.paragraph}>
-          There's a bunch of Yurts available on the property. Each yurt is a large, circular room with 10 beds. Bedding is provided, but privacy is...limited.
-          You are welcome to reserve a spot at no charge, but if possible we would appreciate $10 towards the cost of the rented linens.
-        </p>
+        <div className={styles.section}>
+          <div className={styles.sectionImage}>
+            <Image src={yurt_dwg} alt='drawing of a yurt' width={250} />
+          </div>
+          <div className={styles.sectionContent}>
+            <p className={styles.paragraph}>
+              There's a bunch of Yurts available on the property. Each yurt is a large, circular room with 10 beds. Some of the beds are bunk beds, and it is possible to move them around. Bedding is provided, but privacy is...limited.
+              You are welcome to reserve a spot at no charge, but if possible we would appreciate $10 towards the cost of the rented linens.
+            </p>
+          </div>
+        </div>
+
         <button onClick={handleYurts} className={styles.button}>
           TO RESERVE SPACE IN A YURT, PLEASE CLICK HERE
         </button>
@@ -55,24 +70,46 @@ export default function Stay() {
         </button>
 
         <h2 className={styles.subtitle}>Camping</h2>
-        <p className={styles.paragraph}>
-          Camping is allowed on the property, we have a pretty big area to ourselves. You're welcome to bring your own tent and/or camp in a vehicle.
-        </p>
+        <div className={styles.section}>
+          <div className={styles.sectionImage}>
+            <Image src={tent_dwg} alt='drawing of a tent' width={250} />
+          </div>
+          <div className={styles.sectionContent}>
+            <p className={styles.paragraph}>
+              Camping is allowed on the property, we have a pretty big area to ourselves. You're welcome to bring your own tent and/or camp in a vehicle.
+            </p>
+          </div>
+        </div>
 
         <h2 className={styles.subtitle}>Hotel Rooms</h2>
-        <p className={styles.paragraph}>
-          We've arranged discounted rates at a nearby hotel for those who prefer it. You can just call up This Number: and tell them ______.  They are a ~15 min drive away, but we can't get you to/from the venue; we will set up a groupchat for carpooling.
-        </p>
+        <div className={styles.section}>
+          <div className={styles.sectionImage}>
+            <Image src={motel_dwg} alt='drawing of a motel' width={250} />
+          </div>
+          <div className={styles.sectionContent}>
+            <p className={styles.paragraph}>
+              We've arranged discounted rates at a nearby hotel for those who prefer it. You can just call up This Number: and tell them ______. They are a ~15 min drive away, but we won't be able get you to/from the venue. We will set up a groupchat for carpooling.
+            </p>
+          </div>
+        </div>
 
         <h2 className={styles.subtitle}>Nota Bene!</h2>
-        <p className={styles.paragraph}>
-          Unfortunately the venue rules prohibit dogs inside any buildings -- this means the Yurts and also the reception hall.  Dogs are still allowed on the property otherwise.
-        </p>
+        <div className={styles.section}>
+          <div className={styles.sectionImage}>
+            <Image src={dog_dwg} alt='drawing of dogs' width={250} />
+          </div>
+          <div className={styles.sectionContent}>
+            <p className={styles.paragraph}>
+              Unfortunately the venue rules prohibit dogs inside any buildings -- this means the Yurts and also the reception hall.
+              Dogs are allowed on the property otherwise; tent camping with dogs is OK. Please let us know if you plan to bring a dog.
+            </p>
+          </div>
+        </div>
 
         <br />
         <Link href={'/'} className={styles.link}>Back</Link>
       </div>
-      <CustomPopup 
+      <CustomPopup
         isOpen={isPopupOpen}
         onClose={() => setIsPopupOpen(false)}
         onConfirm={handlePopupConfirm}
