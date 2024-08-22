bf78d8746c29e62ad297fefad5671734afdcf7ee
dedelstein <danhedelstein@gmail.com>
2024-08-22 23:46:35 +0300

    fiddling

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,16 @@ export default function Home() {
   return (
     <main className={styles.mainContainer}>
       <div className={styles.contentContainer}>
-        <div className={styles.imageContainer}>
-          <Image
-            src={wedding_landing}
-            alt="Wedding Landing Image"
-            layout="fill"
-            objectFit="contain"
-            priority
-          />
+        <div className={styles.imageWrapper}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={wedding_landing}
+              alt="Wedding Landing Image"
+              layout="fill"
+              objectFit="contain"
+              priority
+            />
+          </div>
           <div className={styles.buttonContainer}>
             <Link href="/rsvp" className={styles.rsvpButton} aria-label='RSVP' />
             <Link href="/directions" className={styles.directionsButton} aria-label='Directions' />
